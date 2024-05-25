f3af3c801745fa2bf0acbe8ef95491e7f95bab8a
Facu Dell Ubuntu <facundoblancov@gmail.com>
2024-05-25 17:02:40 -0300

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 3000
+  },
   integrations: [tailwind()]
 });
